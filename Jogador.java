d6f783866e893a5e4e3cd004ca731ec1f55e01a0
Paulo Marques <79942696+paulojoseph@users.noreply.github.com>
2021-10-05 18:35:57 -0300

    Update Jogador.java
    Melhorias na Classe Jogador

--- Jogador.java
+++ Jogador.java
@@ -6,23 +6,27 @@ public class Jogador {
   //captura o nome da sala onde o jogador está
 	private static String local_;
   //inventário
-	private static Map<String, Item> inventory_ = new HashMap<String, Item>();
+	private static Map<String, Item> inventario_ = new HashMap<String, Item>();
+  public static Map<String, Objeto> objeto_ = new HashMap<String, Objeto>();
   //pontuação
 	private static int pontos_ = 0;
 
- //seta o jogador para o cenário inicial
+ /*seta o jogador para o cenário inicial
+  *** LEMBRAR SEMPRE ATUALIZAR CASO A SALA INICIAL MUDE DE NOME ***/
 	public Jogador() {
-		local_ = "Salão Inicial";
+		local_ = "lab";
 	}
 
   //cabeçalho do jogo
   public void comecoJogo() {
 
-  System.out.println("\n\n\t\t\t\t\t\t-*- O INÍCIO -*-\n\n");
+  System.out.println("\n\n\t\t\t\t\t-*- O INÍCIO -*-\n\n");
 
   System.out.println("... Você abre os olhos, acordando de um longo e profundo sono. Ainda um pouco atordodado, procura entender o que está acontecendo, sem saber por quanto tempo esteve dormindo ...\n\n");
 
-  System.out.println("... Você está em uma cápsula, que provavelmente se abriu um pouco antes do seu despertar. Na lateral, um aviso denuncia: 'CRYONICS'. Tudo fica mais claro agora, você participou de um experimento de criogenia(criônica). Ao sair da cápsula, em meio à penumbra, você identifica o que parece ser um laboratório, muito bagunçado, com vários papeis espalhados pelo chão, uma janela e uma porta(com acesso por cartão)...");
+  System.out.println("... Você está em uma cápsula, que provavelmente se abriu um pouco antes do seu despertar. Na lateral, um aviso denuncia: 'CRYONICS'. Tudo fica mais claro agora, você participou de um experimento de criogenia(criônica). Ao sair da cápsula, em meio à penumbra, você identifica o que parece ser um laboratório, muito bagunçado, com vários papeis espalhados pelo chão, um crachá, uma janela e uma porta ao norte...\n");
+
+  System.out.println("<<< Digite 'ajuda' a qualquer momento para exibir os principais comandos >>>\n");
 
   }
   //tela de ajuda
@@ -32,9 +36,11 @@ public class Jogador {
 
     System.out.println("Para auxiliá-lo em sua jornada, os seguintes comandos podem ser executados:\n");
 
-    System.out.println("\tOLHAR: Com esse comando você pode ter uma visão geral do ambiente, dos objetos ao redor e possíveis saídas.\n\n"
-    + "\tIR: Comando utilizado para se movimentar até outros ambientes, caso seja possível. Você pode digitar 'IR + Norte' ou apenas 'Norte'. As direções possíveis são 'Norte, Sul, Leste e Oeste'\n\n"
-    + "\tPEGAR: Itens estão espalhados pelos ambientes, alguns deles cruciais para a continuidade da sua jornada. Examine sempre os locais e digite 'PEGAR' + 'nome do item' para adicioná-lo ao inventário.\n");
+    System.out.println(""
+    + "\tVER: Com esse comando você pode ter uma visão geral do ambiente, dos objetos ao redor e possíveis saídas.\n\n"
+    + "\tIR: Comando utilizado para se movimentar até outros ambientes, caso seja possível. Você pode digitar 'IR + Norte' ou apenas 'norte', 'sul', 'leste' e 'oeste'.\n\n"
+    + "\tPEGAR: Itens estão espalhados pelos ambientes, alguns deles são cruciais para a continuidade da sua jornada. Examine sempre os locais e digite 'PEGAR' + 'nome do item' para adicioná-lo ao inventário.\n\n"
+    + "\tUSAR: Utilizar itens que estejam em seu inventário para executar ações em ambientes específicos.\n");
 
   }
 
@@ -54,51 +60,46 @@ public class Jogador {
       case "norte":
 
         if (!temp[0].equals("-")) {
-
           local_ = temp[0];
         } else {
-          System.out.println("Não há um caminho disponível nessa direção. Tente novamente!");
+          msgErroFronteira();
+          break;
         }
-        break;
-
+        
       case "sul":
 
         if (!temp[1].equals("-")) {
-
           local_ = temp[1];
         } else {
-          System.out.println("Não há um caminho disponível nessa direção. Tente novamente!");
+          msgErroFronteira();
+          break;
         }
-        break;
-
+        
       case "leste":
 
         if (!temp[2].equals("-")) {
-
           local_ = temp[2];
         } else {
-          System.out.println("Não há um caminho disponível nessa direção. Tente novamente!");
+          msgErroFronteira();
+          break;
         }
-        break;
 
       case "oeste":
 
         if (!temp[3].equals("-")) {
-
           local_ = temp[3];
         } else {
-          System.out.println("Não há um caminho disponível nessa direção. Tente novamente!");
+          msgErroFronteira();
+          break;
         }
-        break;
       
       default:
-
-        System.out.println("Não há um caminho disponível nessa direção. Tente novamente!");
+        msgErroFronteira();
 		  }
 	}
 
   //método que adiciona item ao inventário
-	public void add(String item, HashMap<String, Sala> salas) {
+	public void addItem(String item, HashMap<String, Sala> salas) {
 
 		//verifica se o objeto está na sua localização atual
 		Sala atual = salas.get(local_);
@@ -110,10 +111,45 @@ public class Jogador {
 			temp = atual.getInventario().get(item);
 
 			atual.getInventario().remove(item);
-			inventory_.put(temp.getNome(), temp);
+			inventario_.put(temp.getNome(), temp);
 			System.out.println(item + " adicionado ao inventário");
 		} else {
 			System.out.println(item + " não está na sala atual");
+		}
+	}
+
+	public void addObjeto(String objeto, HashMap<String, Sala> salas) {
+
+		//verifica se o objeto está na sua localização atual
+		Sala atual = salas.get(local_);
+
+		Objeto temp = null;
+
+    //Sala testeObj = atual.getObjetos().keySet();
+
+		if (atual.getObjetos().containsKey(objeto)) {
+
+      temp = atual.getObjetos().get(objeto);
+
+      atual.getObjetos().remove(objeto);
+      objeto_.put(temp.getNome(), temp);
+
+		}
+	}
+
+	public void usar(String item, HashMap<String, Sala> salas) {
+		Item temp = null;
+
+		if (inventario_.containsKey(item)) { //verifica se o item está no inventário
+			temp = inventario_.get(item);
+			inventario_.remove(item);
+
+			Sala atual = salas.get(local_);
+
+			atual.getInventario().put(temp.getNome(), temp);
+			System.out.println(item + " foi usado com sucesso!");
+		} else { 
+			System.out.println("Desculpe, " + item + " não está no seu inventário.");
 		}
 	}
 
@@ -124,24 +160,41 @@ public class Jogador {
 
   //método que lista os itens do inventário do jogador
 	public void getInventario() {
-		if (inventory_.isEmpty()) {
+		if (inventario_.isEmpty()) {
 
 			System.out.println("O inventário está vazio");
 		} else {
-			for (Map.Entry<String, Item> elt : inventory_.entrySet()) {
-				System.out.println(elt.getKey());
+			for (Map.Entry<String, Item> x : inventario_.entrySet()) {
+				System.out.println(x.getKey());
+			}
+		}
+	}
+
+  //método que lista os itens do inventário do jogador
+	public void getObjetos() {
+		if (objeto_.isEmpty()) {
+
+			System.out.println("Os objetos estão vazios");
+		} else {
+			for (Map.Entry<String, Objeto> y : objeto_.entrySet()) {
+				System.out.println(y.getKey());
 			}
 		}
 	}
 
   //método que descreve um item do inventário
-	public void olhar(String item) {
-    System.out.println(inventory_.get(item).getDescricao());
+	public void ver(String item) {
+    System.out.println(inventario_.get(item).getDescricao());
+	}
+
+  //método que descreve um objeto do inventário
+	public void verObj(String objeto) {
+    System.out.println(objeto_.get(objeto).getDescricao());
 	}
 
   //método que descreve a sala atual
-	public void olhar(HashMap<String, Sala> salas) {
-		salas.get(local_).olhar();
+	public void ver(HashMap<String, Sala> salas) {
+		salas.get(local_).ver();
 	}
 
   //método que captura a pontuação atual
@@ -156,5 +209,9 @@ public class Jogador {
 			pontos.remove(id);
 		}
 	}
-
+  public void msgErroFronteira() {
+    System.out.println("\nNão há um caminho disponível nessa direção. Tente novamente!");
+    
+  }
 }
+
